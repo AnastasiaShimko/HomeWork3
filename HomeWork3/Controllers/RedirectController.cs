12cd025e3a7f048e2fe3ba1b21dd428fdc3fd37b
Anastasia Shimko <anastasiaishimko@gmail.com>
2020-12-08 22:27:29 +0300

    GET: Redirect/ToItAcademy
    Redirect  to https://it-academy.by

---
+++ HomeWork3/Controllers/RedirectController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeWork3.Controllers
+{
+    public class RedirectController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+
+        // GET: Redirect/ToItAcademy
+        public IActionResult ToItAcademy()
+        {
+            return Redirect("https://it-academy.by");
+        }
+    }
+}
