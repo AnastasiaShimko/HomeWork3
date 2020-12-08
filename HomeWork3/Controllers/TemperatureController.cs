d034d5d6c233f261cb3f62c44203ee1b9613e3a3
Anastasia Shimko <anastasiaishimko@gmail.com>
2020-12-08 22:20:24 +0300

    Temperature/GetFTemperatureFromC?cTemp= Endpoind that takes C temperature and converts it to F

--- HomeWork3/Controllers/TemperatureController.cs
+++ HomeWork3/Controllers/TemperatureController.cs
@@ -8,9 +8,16 @@ namespace HomeWork3.Controllers
 {
     public class TemperatureController : Controller
     {
-        public ActionResult Index()
+        public IActionResult Index()
         {
             return Ok();
+        }
+
+        // GET: Temperature/GetFTemperatureFromC?cTemp=32
+        public IActionResult GetFTemperatureFromC(int cTemp)
+        {
+            var fTemp = (cTemp * 9) / 5 + 32;
+            return Content(fTemp.ToString());
         }
     }
 }
